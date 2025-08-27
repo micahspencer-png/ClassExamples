c5882e852f143f1d0380561659a191ce6640c0fd
micahspencer-png <micahspencer@isu.edu>
2025-08-27 08:48:16 -0600

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleFun
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is just a test. It Passed!!! Yay!!!");
+            Console.WriteLine("First Line!!!");
+            Console.WriteLine("Second Line!!!");
+            Console.WriteLine("Third Line!!!");
+            Console.WriteLine("Fourth Line!!!");
+            Console.WriteLine("Yay!!!");
+            Console.Read();
+
+        }
+    }
+}
