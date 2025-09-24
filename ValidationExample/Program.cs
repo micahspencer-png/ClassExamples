cbdfd324cbac2c5bf32c9334031a256e60e1367c
micahspencer-png <micahspencer@isu.edu>
2025-09-24 08:58:01 -0600

    tested validate

--- ValidationExample/Program.cs
+++ ValidationExample/Program.cs
@@ -1,10 +1,40 @@
 ﻿namespace ValidationExample
 {
+    //Take two arguments: a string value to be converted, and an out integer variable to store the result.
+    //Return a boolean indicating if the conversion was successful.
+    //If successful, update the out integer variable with the converted value.
+    //If not, do not update the integer variable.
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.Read();
+            int number = 0;
+            string userInput = Console.ReadLine();
+            ConvertandValidate(userInput,ref number);
+
+            if (ConvertandValidate(userInput,ref number) == true) 
+            {
+                Console.WriteLine($"{number} + 5 = {number + 5}");
+            }
+            else 
+            {
+                Console.WriteLine($"{userInput} isn't a number");
+            }
+
+                Console.Read();
+        }
+
+        static bool ConvertandValidate(string userInput, ref int number)
+        {
+            try
+            {
+                number = int.Parse(userInput);
+                return true;
+            }
+            catch (Exception)
+            { 
+                return false;
+            }
         }
     }
 }
