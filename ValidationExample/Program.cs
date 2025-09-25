2ff57fc5d3ce3a3a7659e4b9334793321577ad27
micahspencer-png <micahspencer@isu.edu>
2025-09-25 08:22:35 -0600

    Fixed validate and set in loop

--- ValidationExample/Program.cs
+++ ValidationExample/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace ValidationExample
+﻿using System.ComponentModel.Design;
+
+namespace ValidationExample
 {
     //Take two arguments: a string value to be converted, and an out integer variable to store the result.
     //Return a boolean indicating if the conversion was successful.
@@ -9,20 +11,38 @@
         static void Main(string[] args)
         {
             int number = 0;
-            string userInput = Console.ReadLine();
-            ConvertandValidate(userInput,ref number);
-
-            if (ConvertandValidate(userInput,ref number) == true) 
+            string userInput;
+            bool quitProgram = false;
+            
+            do
             {
-                Console.WriteLine($"{number} + 5 = {number + 5}");
-            }
-            else 
-            {
-                Console.WriteLine($"{userInput} isn't a number");
-            }
+                Console.WriteLine("Please input a Number. Press Q to Quit");
+                userInput = Console.ReadLine();
+               
+                ConvertandValidate(userInput,ref number);
 
-                Console.Read();
-        }
+                if (userInput == "q" || userInput == "Q")
+                {
+                    quitProgram = true;
+                }
+                else
+                {
+                    if (ConvertandValidate(userInput, ref number) == true)
+                    {
+                        Console.WriteLine($"{number} + 5 = {number + 5}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{userInput} isn't a number");
+                    }
+
+                    
+                }
+            }while (quitProgram == false);
+
+            Console.WriteLine("Press Enter to Quit");
+            Console.Read();
+        }   
 
         static bool ConvertandValidate(string userInput, ref int number)
         {
