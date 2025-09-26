13704dcee04c6c71410fd22788b2be06fa6353db
micahspencer-png <micahspencer@isu.edu>
2025-09-26 08:50:43 -0600

    added draw number. tried to make it work

--- BingoTest/Program.cs
+++ BingoTest/Program.cs
@@ -1,20 +1,38 @@
-﻿namespace BingoTest
+﻿using System.Net.Quic;
+
+namespace BingoTest
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Display();
+            bool quit = false;
+            string userInput;
+            Console.WriteLine("Press enter to draw ball. Type Q to quit.");
+            userInput = Console.ReadLine();
+            
+            do
+            {
+                int row = 0;
+                int column = 0;
 
+                Draw(ref row, ref column);
+                IsDrawn(row, column);
+                Display();
+                Console.Read();
+            } while (quit = false);
+
+            Console.WriteLine("Press Enter to Quit");
             Console.Read();
         }
+
         static void Display() 
         {
             //displays user interface
             string currentLocation = "XX";
             string[] header = { "B","I","N","G","O" };
-            string vDivide = " |";
             int pad = 3;
+            string vDivide =  " |";
             int totalWidth = 5 * (pad + vDivide.Length);
             string hDivide = new string('-', totalWidth);
             
@@ -28,25 +46,46 @@
 
             for (int row = 0; row < 15; row++)
             {
-
-
                 for (int column = 0; column < 5; column++)
                 {
-                    Console.Write(currentLocation.PadLeft(3) + " |");
+                    if (IsDrawn(row,column))
+                    {
+                        currentLocation = "00";
+                    }
+                    else 
+                    {
+                        currentLocation = "XX";
+                    }
+                        Console.Write(currentLocation.PadLeft(pad) + vDivide);
                 }
                 Console.WriteLine();
             }
         }
-        static void Draw() 
+
+        private static bool[,] ballCage= new bool[14, 4];
+
+        static bool IsDrawn(int row, int column)
+        {
+            bool status = false;
+
+            try 
+            { 
+                status = ballCage[row, column];
+            }
+            catch (Exception)
+            {
+                
+            }
+            return status;
+        }
+        static void Draw(ref int row, ref int column) 
         {
             //draws random number
-            bool[,] bingoCage = new bool[4,14];
             Random draw1 = new Random();
             Random draw2 = new Random();
 
-            int num1 = draw1.Next(0,5);
-            int num2 = draw2.Next(0,15);
-
+            row = draw1.Next(0,15);
+            column = draw2.Next(0,5);
         }
 
         static void Check() 
