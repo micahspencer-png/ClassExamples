48260a9a874fbaa23b9029d028742445ee893ab5
micahspencer-png <micahspencer@isu.edu>
2025-10-06 08:52:48 -0600

    added write, append and read file examples

--- WorkingWithFiles/Program.cs
+++ WorkingWithFiles/Program.cs
@@ -4,7 +4,36 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string path = "test.txt";
+
+            //open/create a new file to write text
+            //overwrites existing data
+
+            using (StreamWriter testFile = File.CreateText(path))
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    testFile.WriteLine("This is a test line of text");  
+                }
+            }
+
+            //opens/creates a new file to append text
+            //adds new data to existing data
+            using (StreamWriter testFile = File.AppendText(path))
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    testFile.WriteLine("Append this line of text");
+                }
+            }
+
+            using (StreamReader testFile = new StreamReader(path)) 
+            { 
+                Console.Write(testFile.ReadLine());
+            }
+            // File.AppendText("Hello?");
+
+            Console.Read();
         }
     }
 }
