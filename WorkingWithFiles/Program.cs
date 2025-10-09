4c5ad52d38d24acf9c3c5843c2dc2819dba1411e
micahspencer-png <micahspencer@isu.edu>
2025-10-09 08:53:35 -0600

    further worked on files. added a try catch

--- WorkingWithFiles/Program.cs
+++ WorkingWithFiles/Program.cs
@@ -3,30 +3,30 @@
     internal class Program
     {
         static void WriteFile(string path) 
-        { 
-             //open/create a new file to write text
-            //overwrites existing data
+        {
+            //open / create a new file to write text
+           //overwrites existing data
 
-            //using (StreamWriter testFile = File.CreateText(path))
-            //{
-            //    for (int i = 0; i < 10; i++)
-            //    {
-            //        testFile.WriteLine("This is a line of text");
-            //    }
-            //}
+            using (StreamWriter testFile = File.CreateText(path))
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    testFile.WriteLine("This is a line of text");
+                }
+            }
         }
 
         static void AppendFile(string path) 
         {
             ////opens/creates a new file to append text
             ////adds new data to existing data
-            //using (StreamWriter testFile = File.AppendText(path))
-            //{
-            //    for (int i = 0; i < 10; i++)
-            //    {
-            //        testFile.WriteLine("Append this line of text");
-            //    }
-            //}
+            using (StreamWriter testFile = File.AppendText(path))
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    testFile.WriteLine("Append this line of text");
+                }
+            }
         }
 
         static void ReadFile(string path) 
@@ -34,10 +34,10 @@
             using (StreamReader testFile = new StreamReader(path)) 
             {
                 //returns values in file and empty lines if all values are used
-                //for (int i = 0; i < 30; i++)
-                //{
-                //    Console.WriteLine(testFile.ReadLine());
-                //}
+                for (int i = 0; i < 30; i++)
+                {
+                    Console.WriteLine(testFile.ReadLine());
+                }
 
                 //displays all values in file and stops at end of file
                 do
@@ -47,6 +47,33 @@
             }
         }
 
+        static void AddCustomer(string path, string[] customerData) 
+        { 
+            using (StreamWriter customerDataFile = File.AppendText(path))
+            {
+                string _customerData = "";
+
+                //foreach (string customerRecord in customerData) 
+                //{
+                //    _customerData += customerRecord;
+
+                //}
+                try
+                {
+                    _customerData = $"{customerData[0]}{customerData[1]}{customerData[2]}{customerData[3]}{customerData[4]}";
+                    customerDataFile.WriteLine($"{_customerData}\n");
+                }
+                catch (Exception ex)
+                {
+                    //log record and error description to log file
+                    //get exception message
+                    Console.WriteLine($"{ex.StackTrace}\n\n{customerData.Length}");
+                    
+                }
+
+                
+            }
+        }
         static void Main(string[] args)
         {
             //string path = "test.txt";
@@ -62,13 +89,26 @@
                     {
                         customer = testFile.ReadLine();
                         temp = customer.Split(",");
-                        //Console.Write($"Length : {temp.Length}".PadRight(15)))
-                        foreach (string record in temp)
+                           
+
+                        //Console.Write($"Length : {temp.Length}".PadRight(15));
+
+                        for (int i = 0; i < temp.GetUpperBound(0); i++)
                         {
-                            formatRecord = record.Replace('"', '$');
-                            formatRecord = formatRecord.Replace("$","");
-                            Console.Write(formatRecord.PadRight(15));
+                            temp[i] = temp[i].Replace('"', '$');
+                            temp[i] = temp[i].Replace("$", "");
+                            Console.WriteLine(i + ": " + temp[i]);
                         }
+                        AddCustomer("..\\..\\..\\CustomerDB.txt", temp);
+
+                        //foreach (string record in temp)
+                        //{
+                        //    formatRecord = record.Replace('"', '$');
+                        //    formatRecord = formatRecord.Replace("$","");
+                        //    //Console.Write(formatRecord.PadRight(15));
+
+                        //    Console.WriteLine(record.Count());
+                        //}
                         Console.WriteLine();
 
                     } while (testFile.EndOfStream == false);
