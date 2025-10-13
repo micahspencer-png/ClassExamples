fd2ee67a64bd6c3b82538b6b53001c8b5d3127f5
micahspencer-png <micahspencer@isu.edu>
2025-10-13 08:44:01 -0600

    showed ways to manipulate and create files

--- ListExamples/Program.cs
+++ ListExamples/Program.cs
@@ -4,7 +4,43 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //constructor
+            List<string> names = new List<string>();
+
+            //adds data to list in order
+            names.Add("Jack");
+            names.Add("Jill");
+            names.Add("Bob");
+            names.Add("Mary");
+            names.Add("Sam");
+            names.Add("Derick");
+            names.Add("Steve");
+            names.Add("Martin");
+            names.Add("Selena");
+
+            //writes the index number for "Sam"
+            Console.WriteLine(names.IndexOf("Sam"));
+
+            //inserts new data at specified index
+            //higher indexes shift up
+            names.Insert(4, "Dave");
+
+            //removes an item from list
+            //all indexes below value shift down
+            names.Remove("Jill");
+
+            //removes data at index
+            //all indexes below shift down
+            names.RemoveAt(3);
+
+            //writes all data and their indexes
+            foreach (string name in names)
+            {
+                Console.WriteLine($"{name} is at index: {names.IndexOf(name)}");
+            }
+
+            //pause
+            Console.Read();
         }
     }
 }
