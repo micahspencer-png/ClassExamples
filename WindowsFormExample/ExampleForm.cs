e6a3822e4f4f51c5f5d2f6fad3042d28d416869f
micahspencer-png <micahspencer@isu.edu>
2025-10-14 08:21:56 -0600

    changed name and set up form

--- WindowsFormExample/ExampleForm.cs
+++ WindowsFormExample/ExampleForm.cs
@@ -1,8 +1,8 @@
 namespace WindowsFormExample
 {
-    public partial class Form1 : Form
+    public partial class ExampleForm : Form
     {
-        public Form1()
+        public ExampleForm()
         {
             InitializeComponent();
         }
