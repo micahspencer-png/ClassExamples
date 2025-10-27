7ee71d86784fbf51a599eb3d549d690900c0d6b3
micahspencer-png <micahspencer@isu.edu>
2025-10-27 08:23:56 -0600

    Adjusted names for the program

--- FormControlFunctions/FormControlsFeatures.Designer.cs
+++ FormControlFunctions/FormControlsFeatures.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace FormControlFunctions
 {
-    partial class Form1
+    partial class FormControlsFeatures
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,10 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            SuspendLayout();
+            // 
+            // FormControlsFeatures
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Name = "FormControlsFeatures";
+            Text = "FormControlFeatures";
+            ResumeLayout(false);
         }
 
         #endregion
--- FormControlFunctions/FormControlsFeatures.cs
+++ FormControlFunctions/FormControlsFeatures.cs
@@ -1,8 +1,8 @@
 namespace FormControlFunctions
 {
-    public partial class Form1 : Form
+    public partial class FormControlsFeatures : Form
     {
-        public Form1()
+        public FormControlsFeatures()
         {
             InitializeComponent();
         }
